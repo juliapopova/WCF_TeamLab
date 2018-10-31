41065f767476475006ce15f76bb6fcb50429822b
Dmitriy <muromkin1997@gmail.com>
2018-10-31 20:55:34 +0300

    Adding Files

---
+++ WCF_Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfCalculationLib;
+
+namespace WCF_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            List<double> massA = new List<double>();
+            List<double> massB = new List<double>();
+            for(int i=0;i<n;i++)
+            {
+                massA.Add(1.0);
+                massB.Add(1.0);
+            }
+            CalcService calcservice = new CalcService();
+            InputMatrixes inputMatr = new InputMatrixes();
+            inputMatr.Mass_a = massA.ToArray();
+            inputMatr.Mass_b = massB.ToArray();
+            calcservice.SumMatrixes(inputMatr);
+            OutputMatrixes output = calcservice.SumMatrixes(inputMatr);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WcfCalculationLib/CalcService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfCalculationLib
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class CalcService : ICaclcService
+    {
+        public OutputMatrixes SumMatrixes(InputMatrixes inputMatrixes)
+        {
+            // inputMatrixes.Mass_a
+            OutputMatrixes mass_data = new OutputMatrixes();
+            int n = inputMatrixes.Mass_b.Length;
+            mass_data.Mass_summ = new double[n];
+          //  double[] mass_sum = new double[n];
+            for (int i = 0; i < n; i++)
+                mass_data.Mass_summ[i] = inputMatrixes.Mass_a[i] + inputMatrixes.Mass_b[i];
+
+            return mass_data;
+        }
+    }
+}
