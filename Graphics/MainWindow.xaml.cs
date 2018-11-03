8a4f178d81d3781e69826aa8abf7bc54832e8f5c
Dmitriy <muromkin1997@gmail.com>
2018-11-03 21:57:44 +0300

    Added Gradient Brush

--- Graphics/MainWindow.xaml.cs
+++ Graphics/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WCF_Example;
 
 namespace Graphics
 {
@@ -23,6 +24,39 @@ namespace Graphics
         public MainWindow()
         {
             InitializeComponent();
+            Graphic(c);   
+                        
+        }
+        double c = 0.0001;
+        
+        public void Graphic(double c)
+        {
+            LinearGradientBrush myLinearGradientBrush =
+          new LinearGradientBrush();
+            myLinearGradientBrush.StartPoint = new Point(0, 0);
+            myLinearGradientBrush.EndPoint = new Point(1, 0);
+
+            myLinearGradientBrush.GradientStops.Add(
+                new GradientStop(Colors.Red, 0.0));
+            myLinearGradientBrush.GradientStops.Add(
+                new GradientStop(Colors.LimeGreen, 0.2 + c));
+            myLinearGradientBrush.GradientStops.Add(
+                new GradientStop(Colors.LimeGreen, 0.8 - c));
+            myLinearGradientBrush.GradientStops.Add(
+                new GradientStop(Colors.Red, 1.0));
+            myRect1.Fill = myLinearGradientBrush;
+
+        }
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+           // for (int i = 0; i < 100; i++)
+            //{
+                Graphic(c);
+                c += 0.01;
+            if (c >= 0.3)
+                c = 0.0001;
+               // System.Threading.Thread.Sleep(10);
+               // }
         }
     }
 }
