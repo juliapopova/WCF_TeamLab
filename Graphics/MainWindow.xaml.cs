86c85850568b2f27a521a27e686ca88a6afef1c1
cepera1029 <sergey7182@ya.ru>
2018-11-15 12:27:22 +0300

    program works

--- Graphics/MainWindow.xaml.cs
+++ Graphics/MainWindow.xaml.cs
@@ -12,53 +12,144 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using WCF_Example;
 using System.Drawing;
-using System.Drawing.Drawing2D;
+using WcfCalculationLib;
+using System.Timers;
 
-namespace Graphics
-{
+namespace Graphics {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+    public partial class MainWindow : Window {
+
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        double[,] u = new double[n, n];
+        SolidColorBrush brush = new SolidColorBrush();
+        double clr;
+        Color color;
+
+        public MainWindow() {
+
             InitializeComponent();
-            Graphic(c);   
-                        
-        }
-        double c = 0.0001;
-        
-        public void Graphic(double c)
-        {
-            LinearGradientBrush myLinearGradientBrush =
-          new LinearGradientBrush();
-            myLinearGradientBrush.StartPoint = new Point(0, 0);
-            myLinearGradientBrush.EndPoint = new Point(1, 0);
 
-            myLinearGradientBrush.GradientStops.Add(
-                new GradientStop(Colors.Red, 0.0));
-            myLinearGradientBrush.GradientStops.Add(
-                new GradientStop(Colors.LimeGreen, 0.2 + c));
-            myLinearGradientBrush.GradientStops.Add(
-                new GradientStop(Colors.LimeGreen, 0.8 - c));
-            myLinearGradientBrush.GradientStops.Add(
-                new GradientStop(Colors.Red, 1.0));
-            myRect1.Fill = myLinearGradientBrush;
+            timer.Tick += new EventHandler(timerTick);
+            timer.Interval = new TimeSpan(10);
+
+            //initial 
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    u[i, j] = 5;
+                }
+            }
+        }
+
+        public void initial_plotting() {
+           
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    rects[i, j] = new Rectangle();
+                }
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    rects[i, j].Width = 4;
+                    rects[i, j].Height = 4;
+                    Canvas.SetLeft(rects[i, j], i * 4);
+                    Canvas.SetTop(rects[i, j], j * 4);
+                    Canva.Children.Add(rects[i, j]);
+                }
+            }
+        }
+
+        private void timerTick(object sender, EventArgs e) {
+                calculate();
+        }
+
+        public static T[][] ToJagged<T>(T[,] mArray) {
+            var rows = mArray.GetLength(0);
+            var cols = mArray.GetLength(1);
+            var jArray = new T[rows][];
+            for (int i = 0; i < rows; i++) {
+                jArray[i] = new T[cols];
+                for (int j = 0; j < cols; j++) {
+                    jArray[i][j] = mArray[i, j];
+                }
+            }
+            return jArray;
+        }
+
+
+        public static T[,] ToMultiD<T>(T[][] jArray) {
+            int i = jArray.Count();
+            int j = jArray.Select(x => x.Count()).Aggregate(0, (current, c) => (current > c) ? current : c);
+
+
+            var mArray = new T[i, j];
+
+            for (int ii = 0; ii < i; ii++) {
+                for (int jj = 0; jj < j; jj++) {
+                    mArray[ii, jj] = jArray[ii][jj];
+                }
+            }
+
+            return mArray;
+        }
+        const int n = 75;
+        public Rectangle[,] rects = new Rectangle[n, n];
+
+
+        public void calculate() {
+            double time = 0.2;
+            double tau = 0.1;
+            double h = 1;
+           
+
+            for (int j = 0; j < n; j++)
+                u[0, j] = 500;
+
+            for (int i = 0; i < n; i++)
+                u[i, n - 1] = 500;
+
+            for (int j = 0; j < n; j++)
+                u[n - 1, j] = 300;
+
+            for (int i = 0; i < n; i++)
+                u[i, 0] = 300;
+
+            CalcService calcservice = new CalcService();
+            InputDate inputdate = new InputDate();
+            OutputDate outputDate = new OutputDate();
+            inputdate.H = h;
+            inputdate.Tau = tau;
+            inputdate.Time = time;
+
+            inputdate.Mass_u = ToJagged(u);
+
+            outputDate = calcservice.CulcTeploPosl(inputdate);
+
+            u = ToMultiD(outputDate.Culc_Teplo);
+
+            //отрисовка
+            plotting(u);
+        }
+
+        public void plotting(double[,] u) {
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    clr = (u[i, j] * 255) / 500;
+                    color = Color.FromRgb((byte)clr, 0, (byte)(255 - (int)clr));
+                    brush = new SolidColorBrush(color);
+                    rects[i, j].Fill = brush;
+                    
+                }
+            }
 
         }
-        private void Start_Click(object sender, RoutedEventArgs e)
-        {
-           // for (int i = 0; i < 100; i++)
-            //{
-                Graphic(c);
-                c += 0.01;
-            if (c >= 0.3)
-                c = 0.0001;
-               // System.Threading.Thread.Sleep(10);
-               // }
+        private void Start_Click(object sender, RoutedEventArgs e) {
+            initial_plotting();
+            timer.Start();
+
         }
     }
 }
--- Lib_Thermo/Thermo.cs
+++ Lib_Thermo/Thermo.cs
@@ -55,7 +55,5 @@ namespace Lib_Thermo
 
             return u;
         }
-
     }
-}
 }
